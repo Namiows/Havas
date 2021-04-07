2d2d308ae8bdfb6a6c77d836fc6d6999d6909ac9
Kesley Lahoz <namielahoz@gmail.com>
2021-04-07 10:27:00 -0300

    fixing errors with detailspage

--- posts/src/App.js
+++ posts/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
-import Home from './page/Home';
+import Provider from './context/Provider';
+import Home from './page/Home'
+import PostsDetails from './page/PostsDetails';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <>
-     <Home /> 
+      <BrowserRouter>
+        <Provider>
+          <Switch>
+            <Route exact path='/posts'>
+              <Home />
+            </Route>
+            <Route exact path='/posts/:id'>
+              <PostsDetails />
+            </Route>
+            <Route exact path="/" component={ () => <Redirect to="/posts" /> } />
+          </Switch>
+        </Provider>
+      </BrowserRouter> 
     </>
   );
 }
--- posts/src/components/PostsComponent.jsx
+++ posts/src/components/PostsComponent.jsx
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 
 function PostsComponent ({ element }) {
   
-  const { body, title } = element;
+  const { title } = element;
 
   return (
     <div className='posts-cards'>
       <h4>{ title }</h4>
-      <p>{ body }</p>
     </div>
   );
 }
