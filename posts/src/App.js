4a06d101a80e84e4a6a4b1eadbb7065b4af70135
Kesley Lahoz <namielahoz@gmail.com>
2021-04-07 10:54:34 -0300

    fixing provider problem

--- posts/src/App.js
+++ posts/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
-import Provider from './context/Provider';
 import Home from './page/Home'
 import PostsDetails from './page/PostsDetails';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -10,17 +9,15 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Provider>
-          <Switch>
-            <Route exact path='/posts'>
-              <Home />
-            </Route>
-            <Route exact path='/posts/:id'>
-              <PostsDetails />
-            </Route>
-            <Route exact path="/" component={ () => <Redirect to="/posts" /> } />
-          </Switch>
-        </Provider>
+        <Switch>
+          <Route exact path='/posts'>
+            <Home />
+          </Route>
+          <Route exact path='/posts/:id'>
+            <PostsDetails />
+          </Route>
+          <Route exact path="/" component={ () => <Redirect to="/posts" /> } />
+        </Switch>
       </BrowserRouter> 
     </>
   );
